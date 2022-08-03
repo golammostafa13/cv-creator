1b95fd56110ba5d7da7ca6097a757623208fae08
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-04 01:49:30 +0600

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
-            <h1>CV Creator</h1>
+            <h1>Create Your Own CV</h1>
         </div>
     );
 };
